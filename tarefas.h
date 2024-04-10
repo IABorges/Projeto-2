ed15d194086c8ca9c4805612239b443baa00caa9
gabrielafons <141970054+gabrielafons@users.noreply.github.com>
2024-04-10 16:48:58 +0000

    #define ja criado

--- tarefas.h
+++ tarefas.h
@@ -1,9 +1,11 @@
 #define TOTAL 100
+#define tamanho_descricao 300
+#define tamanho_categoria 100
 
 typedef struct {
     int prioridade;
-    char descricao[300];
-    char categoria[100];
+    char descricao[tamanho_descricao];
+    char categoria[tamanho_categoria];
 } Tarefa;
 
 typedef enum {OK, MAX_TAREFA, SEM_TAREFAS, NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER} ERROS;
